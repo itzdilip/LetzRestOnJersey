49b2ccb681d3b727fd66a7375b624b1906bca8e4
Dilip <itzdilip@hotmail.com>
2014-01-06 11:47:25 +0530

    proxy code

--- src/main/java/com/jodicloud/util/ProxyHelper.java
+++ src/main/java/com/jodicloud/util/ProxyHelper.java
@@ -1,4 +1,6 @@
-package com.jodicloud.common.util;
+package com.jodicloud.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
@@ -6,29 +8,29 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.client.apache.ApacheHttpClient;
 import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
 
-
-
 public class ProxyHelper {
 
-	static String getResourceViaProxy(String proxyURL, String resourceURl) throws Exception{
-	
+	@Autowired
+	private DefaultApacheHttpClientConfig clientConfig;
 
-		DefaultApacheHttpClientConfig clientConfig = new DefaultApacheHttpClientConfig();
+	String getResourceViaProxy(String proxyURL, String resourceURl)
+			throws Exception {
+
+		System.out.println(" Is client config null ?? "
+				+ (clientConfig == null));
+		// DefaultApacheHttpClientConfig clientConfig = new
+		// DefaultApacheHttpClientConfig();
 		clientConfig.getProperties().put(
 				DefaultApacheHttpClientConfig.PROPERTY_PROXY_URI, proxyURL);
 
 		Client client = ApacheHttpClient.create(clientConfig);
-		WebResource webResource = client
-				.resource(resourceURl);
+		WebResource webResource = client.resource(resourceURl);
 
 		ClientResponse response = webResource.accept("application/json").get(
 				ClientResponse.class);
-
-		
 
 		return response.getEntity(String.class);
 
 	}
 
-	
 }
