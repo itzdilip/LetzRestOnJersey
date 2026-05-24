2c0a9aa96ef5c17fcdebee3c6696ea095fcb5f50
smd <dilip.sm>
2026-05-24 20:13:27 +0530

    fix: customize swagger-ui to load local jersey openapi spec

--- src/main/java/com/smd/WebConfig.java
+++ src/main/java/com/smd/WebConfig.java
@@ -16,7 +16,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Map everything else to the WebJar
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/4.18.2/");
+        
+        // Specifically map the initializer to our local file
+        registry.addResourceHandler("/swagger-ui/swagger-initializer.js")
+                .addResourceLocations("classpath:/static/swagger-ui/swagger-initializer.js")
+                .setCachePeriod(0);
     }
 }
